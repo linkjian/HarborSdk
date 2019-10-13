d5fa9568335888110cb7b0fa2521bf150e228694
linjian <linjian@chicv.com>
2019-10-13 14:40:19 +0800

    fixed

---
+++ src/Services/Collection.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: linjian
+ * Date: 2019-04-01
+ * Time: 15:33
+ */
+namespace Harbor\Services;
+
+/**
+ * Class Category
+ * @package Harbor\Services
+ */
+class Collection extends Service
+{
+    protected $endpoint = 'collections';
+}
